b9eeab59e78d38c62a2ee6a9b6d104c5365d68b4
Uberlan Lourenço Torres <uberlan9@gmail.com>
2022-09-19 16:08:24 -0300

    Movimentação da Torre

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,23 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class Program {
-	
+
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch();
 		while (true) {
+			try {
+			UI.clearScreen();
 			UI.printBoard(chessMatch.getPieces());
 			System.out.println();
 			System.out.print("Source: ");
@@ -24,6 +28,15 @@ public class Program {
 			ChessPosition target = UI.readChessPosition(sc);
 			
 			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		    }
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 		
 		
