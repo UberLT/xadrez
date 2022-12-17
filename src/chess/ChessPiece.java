bc5264c80d0c2714b0a96a3cd60df5272eaab6e6
Uberlan Lourenço Torres <uberlan9@gmail.com>
2022-12-17 15:04:23 -0300

    Chess Finalizado

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,19 +4,14 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-public abstract class ChessPiece extends Piece{
-	
+public abstract class ChessPiece extends Piece {
+
 	private Color color;
 	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
-		
-	}
-	
-	public ChessPosition getChessPosition() {
-		return ChessPosition.fromPosition(position);
 	}
 
 	public Color getColor() {
@@ -26,18 +21,21 @@ public abstract class ChessPiece extends Piece{
 	public int getMoveCount() {
 		return moveCount;
 	}
-
-	public void increaseMove() {
+	
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
-	
-	public void idecreaseMove() {
+
+	protected void decreaseMoveCount() {
 		moveCount--;
+	}
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
-	
 }
